92eae4763080387cdd1ce763e5b533ce11986b84
PauloBezz <paulo.bezerra2057@gmail.com>
2024-05-15 19:36:34 -0300

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,13 @@
-import { useState } from "react";
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
 
-import {CORE_CONCEPTS} from "./data.js";
-import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
-import {EXAMPLES} from "./data.js";
-
-function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('components');
-
-  function handleSelect(selectedButton){
-    setSelectedTopic(selectedButton)
-  }
-
-
+export default function App() {
   return (
-    <div>
-      <Header/>     
+    <>
+      <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept 
-            title={CORE_CONCEPTS[0].title}
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]}/>
-            <CoreConcept {...CORE_CONCEPTS[2]}/>
-            <CoreConcept {...CORE_CONCEPTS[3]}/>
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic]}.code</code>
-            </pre>
-          </div>
-
-        </section>
+        <AuthInputs />
       </main>
-    </div>
+    </>
   );
 }
-
-export default App;
