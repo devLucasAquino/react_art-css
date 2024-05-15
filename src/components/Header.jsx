1207432969831a6a2edf41f391beccdf7f27a0f7
PauloBezz <paulo.bezerra2057@gmail.com>
2024-05-15 20:30:16 -0300

    added dynamic and condicionally inline style in AuthInputs.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,7 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p style={{
-        color: 'blue',
-        fontSize: 72
-      }}>A community of artists and art-lovers.</p>
+      <p>A community of artists and art-lovers.</p>
     </header>
   );
 }
