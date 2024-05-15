4e42ad51d0ffc25f3c8fe270f38d38c302557c93
PauloBezz <paulo.bezerra2057@gmail.com>
2024-05-15 20:06:09 -0300

    added inline style at Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,10 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{
+        color: 'blue',
+        fontSize: 72
+      }}>A community of artists and art-lovers.</p>
     </header>
   );
 }
